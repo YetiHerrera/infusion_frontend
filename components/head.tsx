65d0e7a2bab8fd5ebb36e51ef9d05464baf7c9e6
Jonathan Herrera <jonathan.herrera@americavoice.com>
2021-02-16 21:44:43 -0600

    create head and content

--- components/head.tsx
+++ components/head.tsx
@@ -11,24 +11,19 @@ import {
 function Head(): JSX.Element {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#">Contacts</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Toggle />
+      <Navbar.Collapse>
         <Nav className="w-100 mr-5 justify-content-between">
           <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Buscar</Button>
+            <Button>Buscar</Button>
           </Form>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown title="About me" id="dropdown_about">
+            <NavDropdown.Item>Action</NavDropdown.Item>
+            <NavDropdown.Item>Another action</NavDropdown.Item>
+            <NavDropdown.Item>Something</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
+            <NavDropdown.Item>Separated link</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,42 +2,8 @@ export default function Home(): JSX.Element {
   return (
     <div className="container my-5">
       <div className="row">
-        <div className="col-2">
-          <h1>h1</h1>
-        </div>
         <div className="col">
-          <h1>Voice call time all over the world!</h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-2">
-          <h2>h2</h2>
-        </div>
-        <div className="col">
-          <h2>The quick brown fox jumps over the lazy dog.</h2>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-2">
-          <h3>h3</h3>
-        </div>
-        <div className="col">
-          <h3>
-            Send call time balance to more than 1,800 carrier companies around
-            the world, to stay in touch!
-          </h3>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-2">
-          <p>body</p>
-        </div>
-        <div className="col">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-            fringilla sem in tellus placerat luctus. Fusce in massa eu sem
-            semper hendrerit quis a ipsum.
-          </p>
+          <h1>This is the content</h1>
         </div>
       </div>
     </div>
